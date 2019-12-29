99210a4ed10320397110fca8f165030f75318c6a
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-29 02:29:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,39 +3,19 @@ require 'directors_database'
 require 'pp' 
  
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  pp nds
-  
   result = {}
   row_index = 0 
   while row_index < nds.length do
-    names = nds[row_index][:name]
     column_index = 0 
+    director_sum = 0
     while column_index < nds[row_index][:movies].length do
-    result = {names => nds[row_index][:movies][column_index][:world_wide_gross]}
-    
-    #puts directors_database[0][:movies][movie_index][:title]
-    
+    director_sum += nds[row_index][:movies][column_index][:worldwide_gross]
+   # print director_sum
     column_index +=1 
     end
     
+    result[nds[row_index][:name]] =  director_sum
     row_index+=1
-    
   end
   
   
